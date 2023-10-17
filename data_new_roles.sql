d38226b06e34783bb39ceacf4ce29a630fd1eb22
julia0205ro <daria0205ro@gmail.com>
2023-10-17 22:20:37 +0300

    add_data_new_roles

---
+++ data_new_roles.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.new_roles (role_name) VALUES
+	 ('Junior Python developer'),
+	 ('Middle Python developer'),
+	 ('Senior Python developer'),
+	 ('Junior Java developer'),
+	 ('Middle Java developer'),
+	 ('Senior Java developer'),
+	 ('Junior JavaScript developer'),
+	 ('Middle JavaScript developer'),
+	 ('Senior JavaScript developer'),
+	 ('Junior Manual QA engineer');
+INSERT INTO public.new_roles (role_name) VALUES
+	 ('Middle Manual QA engineer'),
+	 ('Senior Manual QA engineer'),
+	 ('Progect Manager'),
+	 ('Designer'),
+	 ('HR'),
+	 ('CEO'),
+	 ('Sales manager'),
+	 ('Junior Automation QA engineer'),
+	 ('Middle Automation QA engineer'),
+	 ('Senior Automation QA engineer');
