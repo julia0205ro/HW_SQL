02d97c27bc0ce1ea18c4f9c3fd77a4ce81343b59
julia0205ro <daria0205ro@gmail.com>
2023-10-17 22:19:13 +0300

    add_data_employee_salary

---
+++ data_employee_salary.sql
@@ -0,0 +1,44 @@
+INSERT INTO public.employee_salary (employee_id,salary_id) VALUES
+	 (8,7),
+	 (1,4),
+	 (5,9),
+	 (40,13),
+	 (23,4),
+	 (11,3),
+	 (52,10),
+	 (15,13),
+	 (26,4),
+	 (16,1);
+INSERT INTO public.employee_salary (employee_id,salary_id) VALUES
+	 (33,7),
+	 (24,7),
+	 (70,16),
+	 (67,8),
+	 (41,6),
+	 (43,9),
+	 (56,1),
+	 (14,5),
+	 (39,5),
+	 (6,7);
+INSERT INTO public.employee_salary (employee_id,salary_id) VALUES
+	 (46,2),
+	 (34,3),
+	 (4,14),
+	 (7,5),
+	 (38,8),
+	 (22,6),
+	 (62,10),
+	 (50,12),
+	 (36,5),
+	 (69,7);
+INSERT INTO public.employee_salary (employee_id,salary_id) VALUES
+	 (79,2),
+	 (72,16),
+	 (87,11),
+	 (92,4),
+	 (86,11),
+	 (78,8),
+	 (82,5),
+	 (100,8),
+	 (99,15),
+	 (88,11);
