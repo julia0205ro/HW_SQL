86098821b928f77ca87b827afa558b517872cc8e
julia0205ro <daria0205ro@gmail.com>
2023-10-17 22:21:37 +0300

    add_data_roles_employee

---
+++ data_roles_employee.sql
@@ -0,0 +1,44 @@
+INSERT INTO public.roles_employee (employee_id,role_id) VALUES
+	 (33,16),
+	 (41,8),
+	 (57,10),
+	 (17,3),
+	 (2,4),
+	 (26,4),
+	 (12,1),
+	 (32,3),
+	 (61,18),
+	 (38,16);
+INSERT INTO public.roles_employee (employee_id,role_id) VALUES
+	 (67,13),
+	 (69,15),
+	 (30,17),
+	 (58,20),
+	 (14,6),
+	 (8,19),
+	 (24,10),
+	 (3,9),
+	 (19,14),
+	 (59,2);
+INSERT INTO public.roles_employee (employee_id,role_id) VALUES
+	 (22,1),
+	 (23,19),
+	 (56,5),
+	 (10,13),
+	 (70,10),
+	 (35,10),
+	 (43,17),
+	 (18,16),
+	 (31,19),
+	 (51,11);
+INSERT INTO public.roles_employee (employee_id,role_id) VALUES
+	 (20,14),
+	 (52,9),
+	 (68,1),
+	 (63,9),
+	 (64,9),
+	 (34,20),
+	 (28,13),
+	 (66,20),
+	 (46,10),
+	 (9,7);
